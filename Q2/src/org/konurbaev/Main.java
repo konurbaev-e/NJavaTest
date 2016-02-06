b873f66417d9f7e29a2d42a6c6a32d88f56021cf
konurbaev-e <forget>
2016-02-06 14:40:30 +0300

    Q2

---
+++ Q2/src/org/konurbaev/Main.java
@@ -0,0 +1,14 @@
+package org.konurbaev;
+
+public class Main {
+    public static void main(String[] args) {
+        recur(99);
+    }
+    public static void recur(int a) {
+        if (a <= 100) {
+            System.out.println("a=" + a);
+            recur(++a);
+            System.out.println("a=" + a);
+        }
+    }
+}
