11aa8cdefe43835fcd9885bf2a4f21e4a12a339c
konurbaev-e <forget>
2016-02-06 22:31:30 +0300

    Q10

---
+++ Q10/README.md
@@ -0,0 +1,21 @@
+# Q10
+Какие из приведенных выражений будут истинными при выполнении кода. Возможно несколько вариантов ответа.
+
+```java
+String a = new String("hello");
+String b = new String(a);
+String c = a;
+char[] d = { 'h', 'e', 'l', 'l', 'o' };
+```
+
+    1) (a == "hello")
+    2) (a == b)
+    3) (a == c)
+    4) a.equals(b)
+    5) a.equals(d)
+
+## Правильный ответ
+3) (a == c)
+
+4) a.equals(b)
+
---
+++ Q10/src/org/konurbaev/Main.java
@@ -0,0 +1,16 @@
+package org.konurbaev;
+
+public class Main {
+    public static void main(String[] str) {
+        String a = new String("hello");
+        String b = new String(a);
+        String c = a;
+        char[] d = { 'h', 'e', 'l', 'l', 'o' };
+
+        System.out.println(a == "hello");
+        System.out.println(a == b);
+        System.out.println(a == c);
+        System.out.println(a.equals(b));
+        System.out.println(a.equals(d));
+    }
+}
