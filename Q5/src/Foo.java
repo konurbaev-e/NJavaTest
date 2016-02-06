597fa634618bc67f6e302738fc999e62c0cb0802
konurbaev-e <forget>
2016-02-06 16:07:16 +0300

    Q5

---
+++ Q5/src/Foo.java
@@ -0,0 +1,12 @@
+public class Foo {
+    private Foo() {
+        return this; // 1
+    }
+    public static Foo get() {
+        return new Foo(); // 2
+    }
+    public static void main(String[] args) {
+        Foo foo1 = get(); // 3
+        Foo foo2 = new Foo(); // 4
+    }
+}
